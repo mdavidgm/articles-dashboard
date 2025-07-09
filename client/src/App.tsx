db70bdb876d3a386250aed97201a5cc23674feee
kumanmc <mdavid29021984@gmail.com>
2025-07-09 22:59:35 +0200

    build(client): Init project with Vite

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <header>
+        <p>Dashboard</p>
+      </header>
+    </>
+  )
+}
+
+export default App
