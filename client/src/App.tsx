4a8ba7b1280bfb25c41dfe131c83aab96b997c16
kumanmc <mdavid29021984@gmail.com>
2025-07-09 23:36:51 +0200

    feat(client): Add Container to style App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css'
+import { Container } from '@mui/material';
 
 function App() {
 
   return (
-    <>
+    <Container maxWidth="lg">
       <header>
         <p>Dashboard</p>
       </header>
-    </>
+    </Container>
   )
 }
 
