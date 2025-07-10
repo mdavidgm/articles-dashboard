b92ddba9fff9cbfab047b9d7536ac5ba0e7ceaeb
kumanmc <mdavid29021984@gmail.com>
2025-07-10 17:50:11 +0200

    fix(client): Fix issue with screen width

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,22 @@
 import Dashboard from './pages/Dashboard';
+import { Container } from '@mui/material';
 
 function App() {
 
-  /*
+  /* cite_start:
     This is the main component of the application.
     It does not contain any state or props either logic.
     So, it does not need to be added to coverage report or tests.
     Anyway, it is tested in e2e tests.
   */
   return (
-    <Dashboard />
-  )
+    <Container
+      maxWidth="lg"
+      disableGutters
+    >
+      <Dashboard />
+    </Container>
+  );
 }
 
-export default App
+export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,9 +6,12 @@ import App from './App.tsx'
   * Entry point is not required to be covered in the  coverage report
   * It renders the main App component into the root element of the HTML.
   * and it tested in e2e tests.
-*/ 
+*/
+import CssBaseline from '@mui/material/CssBaseline';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <CssBaseline />
     <App />
   </StrictMode>,
-)
+);
