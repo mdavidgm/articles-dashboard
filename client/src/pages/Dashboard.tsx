fd66ce6aab58eba32dc07bda0310fd492541b5a7
kumanmc <mdavid29021984@gmail.com>
2025-07-10 17:50:42 +0200

    feat(client): Style Articles Dashboard title

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,16 +1,27 @@
-import { Container } from '@mui/material';
 import HighlightSection from '../components/highlight-section/HighlightSection';
 
-function Dashboard() {
+import { AppBar, Toolbar, Typography, Box } from '@mui/material';
+import DashboardIcon from '@mui/icons-material/Dashboard';
 
+function Dashboard() {
   return (
-    <Container>
-      <header>
-        <p>Dashboard</p>
-      </header>
+    <>
+      <AppBar position="static" color="primary" sx={{ mb: 4 }}>
+        <Toolbar>
+          <DashboardIcon sx={{ mr: 2 }} />
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Articles Dashboard
+          </Typography>
+
+          <Typography variant="subtitle1" component="div">
+            Your Daily Dose of Insights!
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
       <HighlightSection />
-    </Container>
-  )
+    </>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
